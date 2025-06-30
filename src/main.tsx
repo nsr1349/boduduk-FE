e9b4890075b710068f3131b9638d2a43abe67ece
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2025-07-01 00:30:25 +0900

    feat: tanstack query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-)
+  </QueryClientProvider>,
+);
